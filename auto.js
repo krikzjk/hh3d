c24285e94a5a4369bec4a2c93abe61cdd2a854ec
krikzjk <krikz.jk@gmail.com>
2026-02-14 11:50:48 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name          HH3D Auto - Edited by Krizk
 // @namespace     HH3D_Tool_Tampermonkey
-// @version       5.6.7
+// @version       5.6.8
 // @description   Thêm menu tùy chỉnh với các liên kết hữu ích và các chức năng tự động(sửa một chút so với bản gốc)
 // @author        Dr. Trune & Krizk
-// @match         https://hoathinh3d.to/*
+// @match         https://hoathinh3d.la/*
 // @require       https://cdn.jsdelivr.net/npm/sweetalert2@11.26.12/dist/sweetalert2.all.min.js
 // @run-at        document-start
 // @grant         unsafeWindow
